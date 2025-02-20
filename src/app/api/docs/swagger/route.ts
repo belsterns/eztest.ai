282a5b6b0b5c2ee9fd09c5302d5c06b811219673
sanjeevraj-belsterns <sanjeev.r@belsterns.com>
2025-02-20 11:35:11 +0530

    feat(EZ-00): Lint fix

--- src/app/api/docs/swagger/route.ts
+++ src/app/api/docs/swagger/route.ts
@@ -1,6 +1,4 @@
-import { NextRequest } from "next/server";
-
-export async function GET(_req: NextRequest) {
+export async function GET() {
   const html = `<!DOCTYPE html>
     <html lang="en">
     <head>
