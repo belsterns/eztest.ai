049807795f107f627c0bdebea979d3530513efea
sanjeevraj-belsterns <sanjeev.r@belsterns.com>
2025-03-13 14:29:15 +0530

    Bug fix for secret key not found issue in cryptoUtils.ts

--- src/app/backend/utils/cryptoUtils.ts
+++ src/app/backend/utils/cryptoUtils.ts
@@ -1,15 +1,19 @@
 import * as crypto from "crypto";
 
-const SECRET_KEY = process.env.SECRET_KEY!;
 const IV_LENGTH = 16;
 
-if (!SECRET_KEY) throw new Error("Secret key not found");
-
+const getSecretKey = () => {
+  const SECRET_KEY = process.env.SECRET_KEY!;
+ 
+  if (!SECRET_KEY) throw new Error("Secret key not found");
+  return SECRET_KEY;
+}
+  
 export function encryptToken(token: string): string {
   const iv = crypto.randomBytes(IV_LENGTH);
   const cipher = crypto.createCipheriv(
     "aes-256-cbc",
-    Buffer.from(SECRET_KEY, "hex"),
+    Buffer.from(getSecretKey(), "hex"),
     iv,
   );
   const encrypted = cipher.update(token, "utf8", "hex") + cipher.final("hex");
@@ -20,7 +24,7 @@ export function decryptToken(encryptedToken: string): string {
   const [ivHex, encryptedHex] = encryptedToken.split(":");
   const decipher = crypto.createDecipheriv(
     "aes-256-cbc",
-    Buffer.from(SECRET_KEY, "hex"),
+    Buffer.from(getSecretKey(), "hex"),
     Buffer.from(ivHex, "hex"),
   );
   return decipher.update(encryptedHex, "hex", "utf8") + decipher.final("utf8");
