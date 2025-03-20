17b25c33a50dae4a7549fa176c3dbf104177ba78
sanjeevraj-belsterns <sanjeev.r@belsterns.com>
2025-03-20 19:03:51 +0530

    Updated middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,9 @@
 import { NextRequest, NextResponse } from "next/server";
 import { getToken } from "next-auth/jwt";
 
-const frontEndPublicRoutes = ["/login", "/register"];
-const backEndPublicRoutes = ["/api/v1/auth/sign-up"];
-const unprotectedRoutes = ["/about"]; 
+const authRoutes = ["/login", "/register"];
+const frontendUnprotectedRoutes = ["/about"];
+const backendUnprotectedRoutes: string[] = []; 
 
 const defaultUnAuthorizedRoute = "/login";
 const defaultAuthorizedRoute = "/workspaces";
@@ -12,41 +12,29 @@ const secret = process.env.AUTH_SECRET;
 
 export async function middleware(request: NextRequest) {
     const { pathname } = request.nextUrl;
-    const isPublicRoute =
-        frontEndPublicRoutes.includes(pathname) ||
-        backEndPublicRoutes.includes(pathname);
-    const isUnprotectedRoute = unprotectedRoutes.includes(pathname);
 
-    const token = await getToken({ req: request, secret });
-
-    // If the route is unprotected, allow access
-    if (isUnprotectedRoute) {
+    const isApiPublicRoute = backendUnprotectedRoutes.includes(pathname)
+    if(isApiPublicRoute ) {
         return NextResponse.next();
     }
 
-    // Handle the root path `/`
-    if (pathname === "/") {
-        if (token) {
-            return NextResponse.redirect(new URL(defaultAuthorizedRoute, request.url));
-        } else {
-            return NextResponse.redirect(new URL(defaultUnAuthorizedRoute, request.url));
-        }
+    const isPublicRoute = frontendUnprotectedRoutes.includes(pathname)
+    if(isPublicRoute) {
+        return NextResponse.next();
     }
 
-    // If user is NOT authenticated and trying to access a PRIVATE route
-    if (!isPublicRoute && !token) {
-        if (request.nextUrl.pathname.startsWith("/api")) {
-            return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
-        }
+    const token = await getToken({ req: request, secret });
+
+    if(authRoutes.includes(pathname) && token){
+       return NextResponse.redirect(new URL(defaultAuthorizedRoute, request.url));
+    }
+
+    if(pathname.includes('/api') && !token) {
+        return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+    } 
+
+    if(!authRoutes.includes(pathname)  && !token) {
         return NextResponse.redirect(new URL(defaultUnAuthorizedRoute, request.url));
-    }
-
-    // If user IS authenticated and trying to access a PUBLIC route
-    if (isPublicRoute && token) {
-        if (request.nextUrl.pathname.startsWith("/api")) {
-            return NextResponse.json({ message: "Authorized" }, { status: 200 });
-        }
-        return NextResponse.redirect(new URL(defaultAuthorizedRoute, request.url));
     }
 
     return NextResponse.next();
@@ -57,5 +45,3 @@ export const config = {
         "/((?!api/auth|_next/static|_next/image|.*\\.png$).*)",
     ], // Excludes static files and images, but includes API except /api/auth
 };
-
-
