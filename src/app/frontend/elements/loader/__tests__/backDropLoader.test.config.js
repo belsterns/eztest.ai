41e845a4ff112ae1b8f1feac3aa23db612fa4a7b
EZTest AI <eztest.ai@commit.com>
2025-04-07 11:37:36 +0530

    Added src/app/frontend/elements/loader/__tests__/backDropLoader.test.config.js file

---
+++ src/app/frontend/elements/loader/__tests__/backDropLoader.test.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/setupTests.js'],
+  moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx', 'json', 'node'],
+  testPathIgnorePatterns: ['/node_modules/', '/dist/'],
+};
