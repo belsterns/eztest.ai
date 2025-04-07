3d33a15491743ea9b2b8ae402a035ff43d7d6969
EZTest AI <eztest.ai@commit.com>
2025-04-07 11:30:38 +0530

    Added src/app/backend/controllers/workspace/jest.config.js file

---
+++ src/app/backend/controllers/workspace/jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  testEnvironment: 'node',
+  transform: {
+    '^.+\.ts$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'json', 'node'],
+  testRegex: '(/__tests__/.*|(\.|/)(test|spec))\.ts$',
+  coverageDirectory: '<rootDir>/coverage/',
+};
