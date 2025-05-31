1501191b9d043809f96ea2ad65e85182f70a9caf
sanjeevraj-belsterns <sanjeev.r@belsterns.com>
2025-05-31 16:30:08 +0530

    refactor: Improve import organization and formatting in DynamicDrawer and DynamicForm components

--- src/app/frontend/components/formDrawer/drawer.tsx
+++ src/app/frontend/components/formDrawer/drawer.tsx
@@ -1,7 +1,9 @@
 import Drawer from "@mui/material/Drawer";
 import Typography from "@mui/material/Typography";
-import DynamicForm from "./form";
 import { useTheme } from "@mui/material";
+
+import DynamicForm from "./form";
+
 interface DrawerProps {
   open: boolean;
   onClose: () => void;
@@ -12,31 +14,47 @@ interface DrawerProps {
   onSubmit: (data: Record<string, string>) => void;
 }
 
-export default function DynamicDrawer({ open, onClose, module, mode, fields, initialValues = {}, onSubmit }: DrawerProps) {
+export default function DynamicDrawer({
+  open,
+  onClose,
+  module,
+  mode,
+  fields,
+  initialValues = {},
+  onSubmit,
+}: DrawerProps) {
   const theme = useTheme();
 
   return (
-    <Drawer  anchor="right"
-    open={open}
-    onClose={onClose}
-    sx={{
-      width: 350,
-      "& .MuiDrawer-paper": {
-        width: 550,
-        p: 2,
-        backgroundImage:
-          theme.palette.mode === "dark"
-            ? "radial-gradient(at 50% 50%, hsla(210, 100%, 16%, 0.5), hsl(220, 30%, 5%))"
-            : "radial-gradient(ellipse at 50% 50%, hsl(210, 100%, 97%), hsl(0, 0%, 100%))",
-        backgroundRepeat: "no-repeat",
-      },
-    }}>
+    <Drawer
+      anchor="right"
+      open={open}
+      onClose={onClose}
+      sx={{
+        width: 350,
+        "& .MuiDrawer-paper": {
+          width: 550,
+          p: 2,
+          backgroundImage:
+            theme.palette.mode === "dark"
+              ? "radial-gradient(at 50% 50%, hsla(210, 100%, 16%, 0.5), hsl(220, 30%, 5%))"
+              : "radial-gradient(ellipse at 50% 50%, hsl(210, 100%, 97%), hsl(0, 0%, 100%))",
+          backgroundRepeat: "no-repeat",
+        },
+      }}
+    >
       <Typography variant="h6" sx={{ mb: 2 }}>
         {`${mode} ${module}`}
       </Typography>
 
       {/* Dynamic Form - Pass initial values when editing */}
-      <DynamicForm mode={mode} fields={fields} initialValues={initialValues} onSubmit={onSubmit} onClose={onClose} />
+      <DynamicForm
+        mode={mode}
+        fields={fields}
+        initialValues={initialValues}
+        onSubmit={onSubmit}
+        onClose={onClose}
+      />
     </Drawer>
   );
 }
